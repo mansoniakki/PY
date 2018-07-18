4637ab18c3c325f63b8014a0999f0a2933eba24d
YuviLina <mansoniakki@gmail.com>
2018-07-18 17:28:51 +0100

    Added file ex33 and ex35

--- ex33.py
+++ ex33.py
@@ -1,1 +1,16 @@
-print("Working with While Loopp")
+print("Working with While Loop")
+def call_loop(ulimit,incr):
+	i = 0
+	num = []
+	while i < ulimit:
+		print("At the top I is %d " %i)
+		num.append(i)
+		i = i + incr
+		print("Number now", num)
+		print("At the bottom I is %d " %i)
+		print ("Numbers are:")
+	for i in num:
+		print(i)
+print("Calling Function")
+call_loop(20, 2)
+		
---
+++ ex34.py
@@ -0,0 +1,18 @@
+print("#######################################--Working with list elements--#############################")
+animal = []
+animal.append('Zebra')
+print(animal)
+animal.append('Elephant')
+print(animal)
+animal.append('Rabbit')
+print(animal)
+animal.append('Tiger')
+print(animal)
+animal.append('Goat')
+print(animal)
+animal.append('Dog')
+print(animal)
+
+dog = animal[5]
+
+print ("Dog is %s " % dog)
